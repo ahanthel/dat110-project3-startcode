9906784416a7ce64fd8f4b34be5553a87eb5d024
ahanthel <Ahanthel@gmail.com>
2021-04-15 11:46:14 +0200

    task 3

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Util.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/util/Util.java
@@ -42,13 +42,31 @@ public class Util {
 		// it modifies 'upper' and 'id' when lower > upper e.g. set (6, 2) in mod 10 = {6, 7, 8, 9, 0, 1, 2}
 		
 		// implement: read the descriptions above
+		if (lower.compareTo(id) <=0 ){
+
+			if (lower.compareTo(id) <0 ){
+				if (id.compareTo(upper) == 0){
+					lower = lower.add(BigInteger.ONE);
+					return true;
+				}
+				if (id.compareTo(upper) <0 ){
+					lower = lower.add(BigInteger.ONE);
+					upper = upper.add(BigInteger.ONE);
+					return true;
+				}
+
+			}
+			if (id.compareTo(upper)<0){
+				upper = upper.add(BigInteger.ONE);
+				return true;
+			}
+
+			return true;
+		}
 
 
 
-		boolean cond = false;
-
-		
-		return cond;
+		return false;
 	}
 	
 	public static List<String> toString(List<NodeInterface> list) throws RemoteException {
